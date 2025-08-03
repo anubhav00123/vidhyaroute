1f09e9d6b66526b712fc25f883a9f135bcc5c1a8
anubhav00123 <anubhavkaushik2020@gmail.com>
2025-08-03 16:08:22 +0530

    build fixed

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 import Navbar from "@/components/Navbar";
 import VisaCategoriesSection from "@/components/VisaCategoriesSection";
 import HomeBanner from "@/components/HomeBanner";
